4242524dc62cfc8abf41bfe37262bd12ac8b6483
Jonas Dujava <jonas.dujava@gmail.com>
2022-04-03 15:46:03 +0200

    action to verify pdf-A

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,9 @@
 # Container image that runs your code
-FROM alpine:3.10
+FROM java:jre-alpine
 
 COPY verapdf/ /usr/bin/verapdf
 COPY verify /usr/bin/verify
-COPY UK-7987-version1-custom8.xml uk-profile.xml
+COPY UK-7987-version1-custom8.xml /uk-profile.xml
 
 ENV PATH="/usr/bin/verapdf:${PATH}"
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,15 @@
-#!/bin/sh -l
+#!/bin/sh
 
-echo "Hello $1"
-time=$(date)
-echo ::set-output name=time::$time
+set -e
+
+pdf_file="$1"
+working_directory="$2"
+
+if [ -n "$working_directory" ]; then
+  cd "$working_directory"
+fi
+
+verify "$pdf_file"
+
+validationReport=$(verify "$pdf_file" | grep validationReports)
+echo ::set-output name=is_valid::$validationReport
