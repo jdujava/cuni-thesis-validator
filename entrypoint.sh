008ad7acf22a75a90cbd100fa2be666fddbdd407
Jonas Dujava <jonas.dujava@gmail.com>
2022-04-03 15:46:03 +0200

    action to verify pdf-A

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,20 @@
-#!/bin/sh -l
+#!/bin/sh
 
-echo "Hello $1"
-time=$(date)
-echo ::set-output name=time::$time
+set -e
+
+pdf_file="$1"
+working_directory="$2"
+
+if [ -n "$working_directory" ]; then
+  cd "$working_directory"
+fi
+
+verify "$pdf_file"
+
+validationReport=$(verify "$pdf_file" | awk -F'"' '/validationReports/{print $2}')
+echo "::set-output name=is_valid::$validationReport"
+
+if [ "$validationReport" -eq "0" ]; then
+  echo "PDF file is NOT valid!"
+  exit 1
+fi
